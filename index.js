56f6744e303068e67feb2687fb6a1cac628338c9
saeleuterio <saeleuterio@hotmail.com>
2024-08-21 20:32:38 -0300

    JS

---
+++ index.js
@@ -0,0 +1,8 @@
+import retornaProximosFeriados from './servico/retornaFeriados.js';
+import imprimeFeriados from './exibicao/imprimeFeriados.js';
+
+console.log('\nOs próximos feriados serão:\n')
+
+let numeroFeriados = 3;
+
+let proximosFeriados = retornaProximo
---
+++ servico/retornaFeriados.js
@@ -0,0 +1,24 @@
+import feriadosNacionais from '../dados/dataFeriados.js';
+import filtraFeriados from './filtros.js';
+import {formataDiaSemana, formataDataFeriado} from './formataDataFeriados.js';
+
+function retornaProximosFeriados(numeroFeriados){
+    let feriadosFiltro = filtraFeriados(feriadosNacionais, numeroFeriados);
+    
+    let feriadosFiltroFormatados = [];
+    
+    feriadosFiltro.forEach(feriado => {
+        const dataFeriado = feriado.data;
+        let diaSemana = formataDiaSemana(dataFeriado);
+        const dataFeriadoFormatada = formataDataFeriado(dataFeriado);
+        
+        feriadosFiltroFormatados.push({
+            nome: feriado.nome,
+            diaSemana: diaSemana,
+            data: dataFeriadoFormatada
+        });
+    });
+    return feriadosFiltroFormatados;
+}
+
+export default retornaProximosFeriados;
